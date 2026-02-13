91bb62513e8ec8e7c9518f0c638461f86cfe44ec
Edward Brey <edward.brey@toolsay.com>
2026-02-13 07:00:25 -0600

    Template. Build for Android only.

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace maui_set_binding;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace maui_set_binding;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
